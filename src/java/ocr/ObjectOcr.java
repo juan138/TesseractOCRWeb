6fc62671ed394e33d06c86bc9365bae8f42ca112
juan325 <63469749+juan138@users.noreply.github.com>
2021-04-14 15:20:59 -0600

    comit inicial

---
+++ src/java/ocr/ObjectOcr.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ocr;
+
+/**
+ *
+ * @author master
+ */
+public class ObjectOcr {
+    private String base64;
+    private Integer param_x;
+    private Integer param_y;
+    private Integer param_w;
+    private Integer param_h;
+
+    public String getBase64() {
+        return base64;
+    }
+
+    public void setBase64(String base64) {
+        this.base64 = base64;
+    }
+
+    public Integer getParam_x() {
+        return param_x;
+    }
+
+    public void setParam_x(Integer param_x) {
+        this.param_x = param_x;
+    }
+
+    public Integer getParam_y() {
+        return param_y;
+    }
+
+    public void setParam_y(Integer param_y) {
+        this.param_y = param_y;
+    }
+
+    public Integer getParam_w() {
+        return param_w;
+    }
+
+    public void setParam_w(Integer param_w) {
+        this.param_w = param_w;
+    }
+
+    public Integer getParam_h() {
+        return param_h;
+    }
+
+    public void setParam_h(Integer param_h) {
+        this.param_h = param_h;
+    }
+
+}
